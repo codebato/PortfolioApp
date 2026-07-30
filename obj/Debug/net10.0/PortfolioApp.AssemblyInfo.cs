ec6e1b8eea45b389f4e5dc490b4ef1c830a62857
Batihan Eyyubi <batocode@gmail.com>
2026-07-30 12:45:53 +0300

    update

--- obj/Debug/net10.0/PortfolioApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/PortfolioApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab2be4febb735eb951180775d401c9757d1ad85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec49a7f406730605f0de39db937a46a029aafe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
