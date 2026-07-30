dec49a7f406730605f0de39db937a46a029aafe7
Batihan Eyyubi <batocode@gmail.com>
2026-07-30 12:33:31 +0300

    update

--- wwwroot/css/js/site.js
+++ wwwroot/css/js/site.js
@@ -1,33 +1,3 @@
-console.log("SITE.JS ÇALIŞTI");
-
-(function () {
-
-    const savedTheme = localStorage.getItem("theme") || "dark";
-    document.documentElement.setAttribute("data-theme", savedTheme);
-
-    const toggleBtn = document.getElementById("theme-toggle");
-    const icon = document.getElementById("theme-icon");
-
-    function updateIcon(theme) {
-        if (!icon) return;
-        icon.className = theme === "dark"
-            ? "bi bi-sun"
-            : "bi bi-moon-stars";
-    }
-
-    updateIcon(savedTheme);
-
-    if (toggleBtn) {
-        toggleBtn.addEventListener("click", function () {
-
-            const current = document.documentElement.getAttribute("data-theme") || "dark";
-            const next = current === "dark" ? "light" : "dark";
-
-            document.documentElement.setAttribute("data-theme", next);
-            localStorage.setItem("theme", next);
-
-            updateIcon(next);
-        });
-    }
-
-})();
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("ÇALIŞTI");
+});
